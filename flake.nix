311f8d0e37a080f20762d3406e8878e6ab7a47a6
nixos <nixos>
2025-04-08 18:00:21 +0800

    [chore] Implement "nix run" for external debugging

--- flake.nix
+++ flake.nix
@@ -12,14 +12,27 @@
   outputs = {nixpkgs, ...}: let
     pkgs = nixpkgs.legacyPackages.${system};
     system = "x86_64-linux";
+    imagePkg = pkgs.callPackage ./image.nix {};
+    shell =
+      pkgs.writeShellScript "main"
+      ''
+        ${imagePkg} | ${pkgs.lib.getExe pkgs.podman} load
+        ${pkgs.lib.getExe pkgs.podman} run --rm -it -p 0.0.0.0:25555-25556:25555-25556 localhost/warp-proxy:lastest
+      '';
   in {
-    packages.${system} = {default = pkgs.callPackage ./image.nix {};};
+    packages.${system} = {default = imagePkg;};
+
+    apps.${system}.default = {
+      type = "app";
+      program = shell.outPath;
+    };
 
     devShells.${system} = {
       default = pkgs.mkShell {
         packages = with pkgs; [
           alejandra
           nil
+          podman
         ];
       };
     };
